c75be515ca7a8f02f0c76fa37f4df1dadc15e246
Ivan Adrianto <ivanadrianto64@gmail.com>
2020-11-30 09:27:54 +0700

    adding hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route, Switch, NavLink, Link } from 'react-router-dom'
+import { Route, Switch, HashRouter } from 'react-router-dom'
 import Profile from './component/pages/profile/Profile'
 import MovieDetails from './component/pages/movie-details/MovieDetails'
 import DisplayBySearch from './component/pages/search/DisplayBySearch'
@@ -11,23 +11,25 @@ import Homepage from './component/pages/homepage/Homepage'
 class App extends Component {
   render() {
     return (
-      <div>
-        <Switch>
-          <Route exact path='/'>
-            <Homepage />
-          </Route>
-          <Route path="/profile/">
-            <Profile />
-          </Route>
-          <Route path='/search/:keyword'>
-            <DisplayBySearch />
-          </Route>
-          <Route exact path="/movie-details/:id">
-            <MovieDetails />
-          </Route>
-        </Switch>
+      <HashRouter basename='/'>
+        <div>
+          <Switch>
+            <Route exact path='/'>
+              <Homepage />
+            </Route>
+            <Route path="/profile/">
+              <Profile />
+            </Route>
+            <Route path='/search/:keyword'>
+              <DisplayBySearch />
+            </Route>
+            <Route exact path="/movie-details/:id">
+              <MovieDetails />
+            </Route>
+          </Switch>
 
-      </div>
+        </div>
+      </HashRouter >
 
     )
   }
